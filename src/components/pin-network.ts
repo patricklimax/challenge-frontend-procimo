cb453f2f08d2d82606345fe6ec53a908dda8f3db
patricklimax <patricklimax@outlook.com.br>
2024-09-05 10:04:50 -0300

    feat: created pinners marker to map of Network and Station

---
+++ src/components/pin-network.ts
@@ -0,0 +1,7 @@
+import L from 'leaflet';
+import NetworkPin from '../assets/network-blue.gif';
+
+export const PinNetworkIcon = L.icon({
+  iconUrl: NetworkPin,
+  iconSize: [32, 32],
+});
---
+++ src/components/pin-station.ts
@@ -0,0 +1,7 @@
+import L from 'leaflet';
+import StationPin from '../assets/bike.svg';
+
+export const PinStationIcon = L.icon({
+  iconUrl: StationPin,
+  iconSize: [32, 32],
+});
